4909300ea1b0f88e6b205c54fec07cc65561188e
John Paul Narvasa <johnpaulnarvasa6@gmail.com>
2023-02-08 01:03:45 +0800

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci a neque ullam quod, omnis voluptas ea culpa impedit ratione necessitatibus. Dolore eligendi accusamus voluptatibus quod laborum laudantium veniam. Velit beatae natus id? Cupiditate deserunt, pariatur officia similique quasi accusamus, autem omnis ab ut veritatis repudiandae reiciendis dicta illum commodi eius.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci a neque ullam quod, omnis voluptas ea culpa impedit ratione necessitatibus. Dolore eligendi accusamus voluptatibus quod laborum laudantium veniam. Velit beatae natus id? Cupiditate deserunt, pariatur officia similique quasi accusamus, autem omnis ab ut veritatis repudiandae reiciendis dicta illum commodi eius.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci a neque ullam quod, omnis voluptas ea culpa impedit ratione necessitatibus. Dolore eligendi accusamus voluptatibus quod laborum laudantium veniam. Velit beatae natus id? Cupiditate deserunt, pariatur officia similique quasi accusamus, autem omnis ab ut veritatis repudiandae reiciendis dicta illum commodi eius.</p>
+    </article>
+
+</body>
